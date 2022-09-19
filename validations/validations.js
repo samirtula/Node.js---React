7d8fa51e48507bd303a85655ef276d281220bf9c
Samir <samirtula@mail.ru>
2022-09-20 01:26:21 +0300

    refactor+init post validation

--- validations/validations.js
+++ validations/validations.js
@@ -5,4 +5,9 @@ export const registerValidation = [
     body('password', 'Пароль должен быть минимум 5 символов').isLength({min: 5}),
     body('fullName', 'Укажите имя').isLength({min: 3}),
     body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL()
-]
+];
+
+export const loginValidation = [
+    body('email', 'Неверный формат почты').isEmail(),
+    body('password', 'Пароль должен быть минимум 5 символов').isLength({min: 5}),
+];
